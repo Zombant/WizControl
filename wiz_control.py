232c35b826149f38685e897753372b7982beb97d
Anthony Piccinone <anthony.piccinone@gmail.com>
2025-01-30 20:53:29 -0500

    Added status and errors print to stderr

--- wiz_control.py
+++ wiz_control.py
@@ -31,13 +31,13 @@ def send_command(message, ip, port=38899, retries=5):
             response, addr = sock.recvfrom(1024)
             return json.loads(response.decode())
         except socket.timeout:
-            print(f"Timeout occurred, retrying... ({attempt + 1}/{retries})")
+            print(f"Timeout occurred, retrying... ({attempt + 1}/{retries})", file=sys.stderr)
         except Exception as e:
-            print(f"Error sending command: {e}")
+            print(f"Error sending command: {e}", file=sys.stderr)
             return None
 
     sock.close()
-    print(f"Failed to send command after {retries} retries")
+    print(f"Failed to send command after {retries} retries", file=sys.stderr)
     return None
         
 
@@ -100,12 +100,15 @@ def print_help():
            "        device state <on/off>\n"
            "        device rgb <r> <g> <b> [dimmer]\n"
            "        device temp <temp> [dimmer]\n"
-           "        device scene <scene> [dimmer]\n\n"
+           "        device scene <scene> [dimmer]\n"
+           "        device status\n\n"
            "        list devices\n"
            "        list scenes\n")
     sys.exit(1)
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print_help()
     if sys.argv[1] == "list":
         if sys.argv[2] == "devices":
             list_devices()
@@ -116,36 +119,49 @@ if __name__ == "__main__":
         else:
             print_help()
 
-    elif sys.argv[2] == "state":
-        if len(sys.argv) < 4:
-            print_help()
-        if sys.argv[1] not in ip_addresses:
-            print_help()
-        if sys.argv[3] not in ["on", "off"]:
-            print_help()
-        if sys.argv[3] == "on":
-            set_light_state(ip_addresses[sys.argv[1]], True)
-        elif sys.argv[3] == "off":
-            set_light_state(ip_addresses[sys.argv[1]], False)
-        else:
-            print_help()
+    elif len(sys.argv) > 2:
 
-    elif sys.argv[2] == "rgb":
-        if sys.argv[1] not in ip_addresses:
-            print_help()
-        if len(sys.argv) < 6:
-            print_help()
-        dimmer = 100 if len(sys.argv) == 6 else int(sys.argv[6])
-        set_light_rgb(ip_addresses[sys.argv[1]], int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]), dimmer)
+        if sys.argv[2] == "state":
+            if len(sys.argv) < 4:
+                print_help()
+            if sys.argv[1] not in ip_addresses:
+                print_help()
+            if sys.argv[3] not in ["on", "off"]:
+                print_help()
+            if sys.argv[3] == "on":
+                set_light_state(ip_addresses[sys.argv[1]], True)
+            elif sys.argv[3] == "off":
+                set_light_state(ip_addresses[sys.argv[1]], False)
+            else:
+                print_help()
 
-    elif sys.argv[2] == "temp":
-        if sys.argv[1] not in ip_addresses:
-            print_help()
-        dimmer = 100 if len(sys.argv) == 4 else int(sys.argv[4])
-        set_light_temp(ip_addresses[sys.argv[1]], int(sys.argv[3]), dimmer)
+        elif sys.argv[2] == "rgb":
+            if sys.argv[1] not in ip_addresses:
+                print_help()
+            if len(sys.argv) < 6:
+                print_help()
+            dimmer = 100 if len(sys.argv) == 6 else int(sys.argv[6])
+            set_light_rgb(ip_addresses[sys.argv[1]], int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]), dimmer)
 
-    elif sys.argv[2] == "scene":
-        if sys.argv[3] not in scenes:
-            print_help()
-        dimmer = 100 if len(sys.argv) == 4 else int(sys.argv[4])
-        set_light_scene(ip_addresses[sys.argv[1]], scenes[sys.argv[3]], dimmer)
+        elif sys.argv[2] == "temp":
+            if sys.argv[1] not in ip_addresses:
+                print_help()
+            dimmer = 100 if len(sys.argv) == 4 else int(sys.argv[4])
+            set_light_temp(ip_addresses[sys.argv[1]], int(sys.argv[3]), dimmer)
+
+        elif sys.argv[2] == "scene":
+            if sys.argv[3] not in scenes:
+                print_help()
+            dimmer = 100 if len(sys.argv) == 4 else int(sys.argv[4])
+            set_light_scene(ip_addresses[sys.argv[1]], scenes[sys.argv[3]], dimmer)
+        
+        elif sys.argv[2] == "status":
+            if sys.argv[1] not in ip_addresses:
+                print_help()
+            
+            status = get_light_status(ip_addresses[sys.argv[1]])
+            
+            print(json.dumps(status, indent=4))
+
+    else:
+        print_help()
